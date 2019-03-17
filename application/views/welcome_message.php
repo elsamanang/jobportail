09cf176f9560fbb1e3c26dce6a6486f30180755a
Roland Beni <roland.beni1@gmail.com>
2019-03-18 00:36:43 +0200

    [UPDATE]correct site_url and defined a provisoir accueil

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,18 +68,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Welcome to JobPortal!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<nav>
+			<ul>
+				<li><a href="#">Login</a></li>
+				<li><a href="<?php echo base_url('demandeur/create')?>">Inscription Demandeur</a></li>
+				<li><a href="<?php echo base_url('employeur/create')?>">Inscription Employeur</a></li>
+			</ul>
+		</nav>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
