43a233b20f33302dc43513e13de99d1b0867c35d
Roland Beni <roland.beni1@gmail.com>
2019-03-18 22:50:03 +0200

    [UPDATE] define methode for image upload

--- application/controllers/Demandeur.php
+++ application/controllers/Demandeur.php
@@ -52,36 +52,85 @@ class Demandeur extends CI_Controller
         } else {
             $pwd = $this->input->post('pwd');
             $pwdconf = $this->input->post('pwdconf');
-            if ($pwd == $pwdconf){
-                $data = array(
-                    'nomDemandeur' => $this->input->post('nomDemandeur',TRUE),
-                    'prenomDemandeur' => $this->input->post('prenomDemandeur',TRUE),
-                    'titre' => $this->input->post('titre',TRUE),
-                    'adresseDemandeur' => $this->input->post('adresseDemandeur',TRUE),
-                    'emailDemandeur' => $this->input->post('emailDemandeur',TRUE),
-                    'telephoneDemandeur' => $this->input->post('telephoneDemandeur',TRUE),
-                    'genre' => $this->input->post('genre',TRUE),
-                    'dateNaissance' => $this->input->post('dateNaissance',TRUE),
-                    'nationalite' => $this->input->post('nationalite',TRUE),
-                    'etatCivil' => $this->input->post('etatCivil',TRUE),
-                    'imageProfile' => $this->input->post('imageProfile',TRUE),
-                    'pseudo' => $this->input->post('pseudo',TRUE),
-                    'pwd' => sha1($this->input->post('pwd',TRUE))
-                );
-                try{
-                    $this->demandeur_model->insert($data);
-                    $this->session->set_flashdata('message', '<p style="color:green;">Create Record Success</p?>');
-                    redirect(site_url('demandeur/create'));
-                }catch (Exception $e){
-                    $this->session->set_flashdata('message', '<p style="color:red;">Create Record Failed >>'.$e.'</p>');
+            $email = $this->input->post('emailDemandeur',TRUE);
+            $pseudo = $this->input->post('pseudo',TRUE);
+            $verifEmail = $this->demandeur_model->get_by_email($email);
+            $verifPseudo = $this->demandeur_model->get_by_pseudo($pseudo);
+            if(empty($verifEmail)){
+                if(empty($verifPseudo)){
+                    if ($pwd == $pwdconf){
+                        if($_FILES['imageProfile']['name'] != '')  
+                            $profile = $this->upload_image();
+                        $data = array(
+                            'nomDemandeur' => $this->input->post('nomDemandeur',TRUE),
+                            'prenomDemandeur' => $this->input->post('prenomDemandeur',TRUE),
+                            'titre' => $this->input->post('titre',TRUE),
+                            'adresseDemandeur' => $this->input->post('adresseDemandeur',TRUE),
+                            'emailDemandeur' => $this->input->post('emailDemandeur',TRUE),
+                            'telephoneDemandeur' => $this->input->post('telephoneDemandeur',TRUE),
+                            'genre' => $this->input->post('genre',TRUE),
+                            'dateNaissance' => $this->input->post('dateNaissance',TRUE),
+                            'nationalite' => $this->input->post('nationalite',TRUE),
+                            'etatCivil' => $this->input->post('etatCivil',TRUE),
+                            'imageProfile' => $profile,
+                            'pseudo' => $this->input->post('pseudo',TRUE),
+                            'pwd' => sha1($this->input->post('pwd',TRUE))
+                        );
+                        try{
+                            $this->demandeur_model->insert($data);
+                            $this->session->set_flashdata('message', '<p style="color:green;">Create Record Success</p?>');
+                            redirect(site_url('demandeur/create'));
+                        }catch (Exception $e){
+                            $this->session->set_flashdata('message', '<p style="color:red;">Create Record Failed >>'.$e.'</p>');
+                            redirect(site_url('demandeur/create'));
+                        }
+                        
+                    }else{
+                        $this->session->set_flashdata('message', '<p style="color:red;">Password not much</p>');
+                        redirect(site_url('demandeur/create'));
+                    }
+                }else{
+                    $this->session->set_flashdata('message', '<p style="color:red;">Ce Pseudo est deja attribué</p>');
                     redirect(site_url('demandeur/create'));
                 }
-                
             }else{
-                $this->session->set_flashdata('message', '<p style="color:red;">Password not much</p>');
+                $this->session->set_flashdata('message', '<p style="color:red;">Cet Email existe deja</p>');
                 redirect(site_url('demandeur/create'));
-            }  
+            }
+              
         } 
+    }
+
+    public function upload_image(){
+        if ($_FILES['imageProfile']['size'] <= 10240000){
+
+            $url="/uploads/demandeur";
+            $image=basename($_FILES['imageProfile']['name']);
+            $image=str_replace(' ','|',$image);
+            $type=explode(".",$image);
+            $type=$type[count($type)-1];
+
+            if(in_array($type,array('jpg','jpeg','png','JPG','JPEG','PNG')))
+            {
+                $tmppath="uploads/demandeur/".$this->input->post('nomDemandeur',TRUE).".".$type;
+                if(is_uploaded_file($_FILES["imageProfile"]["tmp_name"]))
+                {
+                    move_uploaded_file($_FILES['imageProfile']['tmp_name'],$tmppath);
+                    return $tmppath;
+                }
+            }
+            else{
+                
+                $error = '<p style="color:red;">Format invalide, seul les formats: JPEG, PNG sont autorisés</p>';
+                $this->session->set_flashdata('message', $error);
+                redirect(site_url('employeur/create'));  
+            }
+        }
+        else{
+            $error = '<p style="color:red;">Taille invalide, importez un fichier de taille inférieur à 100ko</p>';
+            $this->session->set_flashdata('message', $error);
+            redirect(site_url('employeur/create'));
+        }
     }
 
     public function _rules() {
--- application/views/ajout_demandeur.php
+++ application/views/ajout_demandeur.php
@@ -71,7 +71,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<h1><?php echo $title;?></h1>
     <?php if(isset($this->session->message)) echo $this->session->message;?>
 	<div id="body">
-        <?php echo form_open('demandeur/create_action','class ="form-horizontal form-label-left"');?>
+        <?php echo form_open_multipart('demandeur/create_action','class ="form-horizontal form-label-left"');?>
             <div class="form-group">
                 <label for="varchar">NomDemandeur <?php echo form_error('nomDemandeur') ?></label>
                 <input type="text" class="form-control" name="nomDemandeur" id="nomDemandeur" placeholder="NomDemandeur"/>
