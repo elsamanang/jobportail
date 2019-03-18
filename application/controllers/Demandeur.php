22c1db33395f7060669f4d7d49ea85327636a7ef
Roland Beni <roland.beni1@gmail.com>
2019-03-18 09:54:53 +0200

    [UPDATE] index method that call list of demandeur

--- application/controllers/Demandeur.php
+++ application/controllers/Demandeur.php
@@ -9,6 +9,38 @@ class Demandeur extends CI_Controller
         parent::__construct();
     }
 
+    public function index()
+    {
+        $q = urldecode($this->input->get('q', TRUE));
+        $start = intval($this->input->get('start'));
+        
+        if ($q <> '') {
+            $config['base_url'] = base_url() . 'demandeur/index.php?q=' . urlencode($q);
+            $config['first_url'] = base_url() . 'demandeur/index.php?q=' . urlencode($q);
+        } else {
+            $config['base_url'] = base_url() . 'demandeur/index.php';
+            $config['first_url'] = base_url() . 'demandeur/index.php';
+        }
+
+        $config['per_page'] = 10;
+        $config['page_query_string'] = TRUE;
+        $config['total_rows'] = $this->demandeur_model->total_rows($q);
+        $demandeur = $this->demandeur_model->get_limit_data($config['per_page'], $start, $q);
+
+        $this->load->library('pagination');
+        $this->pagination->initialize($config);
+
+        $data = array(
+            'demandeur_data' => $demandeur,
+            'q' => $q,
+            'pagination' => $this->pagination->create_links(),
+            'total_rows' => $config['total_rows'],
+            'start' => $start,
+        );
+        $data['title']= "Liste des Demandeur";
+        $this->load->view('list_demandeur', $data);
+    }
+
     public function create(){
         $data['title'] = "Ajout Demandeur";
         $this->load->view('ajout_demandeur', $data);
