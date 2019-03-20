f1de092af1409d5826129c41436af9b07c1bfb51
Roland Beni <roland.beni1@gmail.com>
2019-03-20 23:41:14 +0200

    [UPDATE] ajout du signe indiquant les champs obligatoires

--- application/views/inscription.php
+++ application/views/inscription.php
@@ -9,26 +9,26 @@
                     <div class="row">
                         <div class="input-field col s6">
                             <i class="material-icons prefix">business</i>
-                            <input id="icon_name" type="text" class="validate" name="nomEmployeur">
-                            <label for="icon_name">Nom Entreprise <?php echo form_error('nomEmployeur') ?></label>
+                            <input id="icon_name" type="text" class="validate" name="nomEmployeur" required>
+                            <label for="icon_name">Nom Entreprise <span class="red-text">*<span></label>
                             <span><?php echo form_error('nomEmployeur') ?></span> 
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">location_on</i>
-                            <input id="icon_adresse" type="text" class="validate" name="adresseEmployeur">
-                            <label for="icon_adresse">Adresse</label>
+                            <input id="icon_adresse" type="text" class="validate" name="adresseEmployeur" required>
+                            <label for="icon_adresse">Adresse <span class="red-text">*<span></label>
                             <?php echo form_error('adresseEmployeur') ?>
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">mail_outline</i>
-                            <input id="icon_email" type="email" class="validate" name="emailEmployeur">
-                            <label for="icon_email">Email</label>
+                            <input id="icon_email" type="email" class="validate" name="emailEmployeur" required>
+                            <label for="icon_email">Email <span class="red-text">*<span></label>
                             <?php echo form_error('emailEmployeur') ?>
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">phone</i>
-                            <input id="icon_telephone" type="tel" class="validate" name="telephoneEmployeur">
-                            <label for="icon_telephone">Telephone</label>
+                            <input id="icon_telephone" type="tel" class="validate" name="telephoneEmployeur" required>
+                            <label for="icon_telephone">Telephone <span class="red-text">*<span></label>
                             <?php echo form_error('telephoneEmployeur') ?>
                         </div>
                         <div class="input-field col s6">
@@ -67,19 +67,20 @@
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">lock</i>
-                            <input id="icon_pwd" type="password" class="validate" name="pwd">
-                            <label for="icon_pwd">Password</label>
+                            <input id="icon_pwd" type="password" class="validate" name="pwd" required>
+                            <label for="icon_pwd">Password <span class="red-text">*<span></label>
                             <?php echo form_error('pwd') ?>
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">lock</i>
-                            <input id="icon_pwdconf" type="password" class="validate" name="pwdconf">
-                            <label for="icon_pwdconf">Password-Confirm</label>
+                            <input id="icon_pwdconf" type="password" class="validate" name="pwdconf" required>
+                            <label for="icon_pwdconf">Password-Confirm <span class="red-text">*<span></label>
                             <?php echo form_error('pwdconf') ?>
                         </div>
                     </div>
                     <button class="btn waves-effect waves-light center pulse" type="submit" name="logup" id="" value="S'inscrire">S'inscrire</button>
                 </form>
+                <span class="red-text">(*) <strong style="font-size :12px;">champs obligatoire</strong><span>
             </div>
         </div>
     </div>
@@ -92,66 +93,66 @@
                     <div class="row">
                         <div class="input-field col s6">
                             <i class="material-icons prefix">person</i>
-                            <input id="icon_nom" type="text" class="validate" name="nomDemandeur">
-                            <label for="icon_nom">Nom </label>
+                            <input id="icon_nom" type="text" class="validate" name="nomDemandeur" required>
+                            <label for="icon_nom">Nom <span class="red-text">*<span></label>
                             <?php echo form_error('nomDemandeur') ?>
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">person</i>
-                            <input id="icon_prenom" type="text" class="validate" name="prenomDemandeur">
-                            <label for="icon_prenom">Prenom </label>
+                            <input id="icon_prenom" type="text" class="validate" name="prenomDemandeur" required>
+                            <label for="icon_prenom">Prenom <span class="red-text">*<span></label>
                             <?php echo form_error('prenomDemandeur') ?>
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">school</i>
-                            <input id="icon_titre" type="text" class="validate" name="titre">
-                            <label for="icon_titre">Titre </label>
+                            <input id="icon_titre" type="text" class="validate" name="titre" required>
+                            <label for="icon_titre">Titre <span class="red-text">*<span></label>
                             <?php echo form_error('titre') ?>
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">location_on</i>
-                            <input id="icon_adress" type="text" class="validate" name="adresseDemandeur">
-                            <label for="icon_adress">Adresse</label>
+                            <input id="icon_adress" type="text" class="validate" name="adresseDemandeur" required>
+                            <label for="icon_adress">Adresse <span class="red-text">*<span></label>
                             <?php echo form_error('adresseDemandeur') ?>
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">mail_outline</i>
-                            <input id="icon_mail" type="email" class="validate" name="emailDemandeur">
-                            <label for="icon_mail">Email</label>
+                            <input id="icon_mail" type="email" class="validate" name="emailDemandeur" required>
+                            <label for="icon_mail">Email <span class="red-text">*<span></label>
                             <?php echo form_error('emailDemandeur') ?>
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">phone</i>
-                            <input id="icon_phone" type="tel" class="validate" name="telephoneDemandeur">
-                            <label for="icon_phone">Telephone</label>
+                            <input id="icon_phone" type="tel" class="validate" name="telephoneDemandeur" required>
+                            <label for="icon_phone">Telephone <span class="red-text">*<span></label>
                             <?php echo form_error('telephoneDemandeur') ?>
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">face</i>
-                            <select name="genre" >
+                            <select name="genre" required>
                                 <option value="f">Feminin</option>
                                 <option value="m">Masculin</option>
                                 <option value="a">Autres</option>
                             </select>
-                            <label>Genre</label>
+                            <label>Genre <span class="red-text">*<span></label>
                             <?php echo form_error('genre') ?>
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">cake</i>
-                            <input type="text" class="datepicker" placeholder="Date de Naissance" id="dateNaissance" name="dateNaissance">
-                            <label for="dateNaissance">Date Naissance</label>
+                            <input type="text" class="datepicker" placeholder="Date de Naissance" id="dateNaissance" name="dateNaissance" required>
+                            <label for="dateNaissance">Date Naissance <span class="red-text">*<span></label>
                             <?php echo form_error('dateNaissance') ?>
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">public</i>
-                            <input id="icon_nation" type="text" class="validate" name="nationalite">
-                            <label for="icon_nation">Nationalite</label>
+                            <input id="icon_nation" type="text" class="validate" name="nationalite" required>
+                            <label for="icon_nation">Nationalite <span class="red-text">*<span></label>
                             <?php echo form_error('nationalite') ?>
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">local_post_office</i>
-                            <input id="icon_civil" type="text" class="validate" name="etatCivil">
-                            <label for="icon_civil">Etat Civil</label>
+                            <input id="icon_civil" type="text" class="validate" name="etatCivil" required>
+                            <label for="icon_civil">Etat Civil <span class="red-text">*<span></label>
                             <?php echo form_error('etatCivil') ?>
                         </div>
                         <div class="file-field input-field col s6">
@@ -172,19 +173,20 @@
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">lock</i>
-                            <input id="icon_passwd" type="password" class="validate" name="pwd">
-                            <label for="icon_passwd">Password</label>
+                            <input id="icon_passwd" type="password" class="validate" name="pwd" required>
+                            <label for="icon_passwd">Password <span class="red-text">*<span></label>
                             <?php echo form_error('pwd') ?>
                         </div>
                         <div class="input-field col s6">
                             <i class="material-icons prefix">lock</i>
-                            <input id="icon_passwdconf" type="password" class="validate" name="pwdconf">
-                            <label for="icon_passwdconf">Password-Confirm</label>
+                            <input id="icon_passwdconf" type="password" class="validate" name="pwdconf" required>
+                            <label for="icon_passwdconf">Password-Confirm <span class="red-text">*<span></label>
                             <?php echo form_error('pwdconf') ?>
                         </div>
                     </div>
                     <button class="btn waves-effect waves-light center pulse" type="submit" name="logup" id="" value="S'inscrire">S'inscrire</button>
                 </form>
+                <span class="red-text">(*) <strong style="font-size :12px;">champs obligatoire</strong><span>
             </div>
         </div>
     </div>
--- application/views/login.php
+++ application/views/login.php
@@ -8,16 +8,16 @@
                         <div class="row">
                             <div class="input-field col s12">
                                 <i class="material-icons prefix">mail_outline</i>
-                                <input id="icon_prefix" type="email" class="validate" name="email">
-                                <label for="icon_prefix">Email</label>
+                                <input id="icon_prefix" type="email" class="validate" name="email" required>
+                                <label for="icon_prefix">Email <span class="red-text">*<span></label>
                             </div>
                             <!-- <span class="input-field col s12 white-text center red"><?php echo form_error('email') ?></span> -->
                         </div>
                         <div class="row">
                             <div class="input-field col s12">
                                 <i class="material-icons prefix">lock</i>
-                                <input id="icon_telephone" type="password" class="validate" name="pwd">
-                                <label for="icon_telephone">Password</label>
+                                <input id="icon_telephone" type="password" class="validate" name="pwd" required>
+                                <label for="icon_telephone">Password <span class="red-text">*<span></label>
                             </div>
                             <!-- <span class="input-field col s12"><?php echo form_error('pwd') ?></span> -->
                             <div class="left">
@@ -34,6 +34,7 @@
                         </div>
                         <button class="btn waves-effect waves-light center" type="submit" name="login" id="" value="SeConnecter">Se Connecter</button>
                     </form>
+                    <span class="red-text">(*) <strong style="font-size :12px;">champs obligatoire</strong><span>
                 </div>
             </div>
         </div>
