3645f4aa9a79d7c87d2381e0ea3a62ee33fe8562
Roland Beni <roland.beni1@gmail.com>
2019-03-21 01:27:06 +0200

    [ADD] view de modification du profile et autres informations

---
+++ application/views/modif_uprofile.php
@@ -0,0 +1,235 @@
+<?php 
+    var_dump($user);
+    //il ne reste qu'a remplir le value de user dans les inputs!
+?>
+<h4 class="center blue-grey-text"><?php echo ucfirst($title);?></h4>
+<div class="row">
+    <div class="col s12 m6 offset-m1">
+        <div class="col s12 m12">
+            <div class="card">
+                <div class="card-content black-text">
+                    <form class="col s12">
+                        <div class="row">
+                            <div class="input-field col s6">
+                                <i class="material-icons prefix">person</i>
+                                <input id="icon_nom" type="text" class="validate">
+                                <label for="icon_nom">Nom </label>
+                            </div>
+                            <div class="input-field col s6">
+                                <i class="material-icons prefix">person</i>
+                                <input id="icon_prenom" type="text" class="validate">
+                                <label for="icon_prenom">Prenom </label>
+                            </div>
+                            <div class="input-field col s6">
+                                <i class="material-icons prefix">school</i>
+                                <input id="icon_titre" type="text" class="validate">
+                                <label for="icon_titre">Titre </label>
+                            </div>
+                            <div class="input-field col s6">
+                                <i class="material-icons prefix">location_on</i>
+                                <input id="icon_adress" type="text" class="validate">
+                                <label for="icon_adress">Adresse</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <i class="material-icons prefix">mail_outline</i>
+                                <input id="icon_mail" type="email" class="validate">
+                                <label for="icon_mail">Email</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <i class="material-icons prefix">phone</i>
+                                <input id="icon_phone" type="tel" class="validate">
+                                <label for="icon_phone">Telephone</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <i class="material-icons prefix">face</i>
+                                <select>
+                                    <option value="f">Feminin</option>
+                                    <option value="m">Masculin</option>
+                                    <option value="a">Autres</option>
+                                </select>
+                                <label>Genre</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <i class="material-icons prefix">cake</i>
+                                <input type="text" class="datepicker" placeholder="Date de Naissance" id="dateNaissance">
+                                <label for="dateNaissance">Date Naissance</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <i class="material-icons prefix">public</i>
+                                <input id="icon_nation" type="text" class="validate">
+                                <label for="icon_nation">Nationalite</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <i class="material-icons prefix">local_post_office</i>
+                                <input id="icon_civil" type="text" class="validate">
+                                <label for="icon_civil">Etat Civil</label>
+                            </div>
+                            <div class="file-field input-field col s6">
+                                <div class="btn">
+                                    <span>File</span>
+                                    <input type="file">
+                                </div>
+                                <div class="file-path-wrapper">
+                                    <input class="file-path validate" type="text" placeholder="Image de Profile">
+                                </div>
+                            </div>
+                            <div class="input-field col s6">
+                                <i class="material-icons prefix">perm_identity</i>
+                                <input id="icon_pseud" type="text" class="validate">
+                                <label for="icon_pseud">Pseudo</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <i class="material-icons prefix">lock</i>
+                                <input id="icon_passwd" type="password" class="validate">
+                                <label for="icon_passwd">Password</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <i class="material-icons prefix">lock</i>
+                                <input id="icon_passwdconf" type="password" class="validate">
+                                <label for="icon_passwdconf">Password-Confirm</label>
+                            </div>
+                        </div>
+                    </form>
+                    <a class="waves-effect waves-light btn center pulse">Update</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m5">
+        <div class="col s12 m12">
+            <div class="card">
+                <div class="card-content">
+                    <h5>Formation</h5>
+                    <div class="divider"></div>
+                    <table class="responsive-table highlight">
+                        <thead>
+                        <tr>
+                            <th>Formation</th>
+                            <th>Institution</th>
+                            <th>Duree</th>
+                            <th>Diplome</th>
+                            <th>Resultat</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        <?php foreach ($formations as $formation) {?>
+                        <tr>
+                            <td><?php echo ucfirst($formation->nomFormation);?></td>
+                            <td><?php echo ucfirst($formation->nomInstitution);?></td>
+                            <td><?php echo $formation->dateDebutFormation;?>-<?php echo $formation->dateFinFormation;?></td>
+                            <td><?php echo ucfirst($formation->diplomeFormation);?></td>
+                            <td><?php echo ucfirst($formation->resultatFormation);?></td>
+                            <td>
+                                <a class="btn-floating blue darken-1 center" href="modif_formation.html"><i class="material-icons">edit</i></a>
+                                <a class="btn-floating red darken-1 center" href=""><i class="material-icons">delete_sweep</i></a>
+                            </td>
+                        </tr>
+                        <?php }?>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+        <div class="col s12 m12">
+            <div class="card">
+                <div class="card-content">
+                    <h5>Competences</h5>
+                    <div class="divider"></div>
+                    <table class="responsive-table highlight">
+                        <thead>
+                        <tr>
+                            <th>Competence</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        <?php foreach ($competences as $competence) {?>
+                        <tr>
+                            <td><?php echo ucfirst($competence->nomCompetence);?></td>
+                            <td>
+                                <a class="btn-floating blue darken-1 center" href="modif_competences.html"><i class="material-icons">edit</i></a>
+                                <a class="btn-floating red darken-1 center" href=""><i class="material-icons">delete_sweep</i></a>
+                            </td>
+                        </tr>
+                        <?php }?>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+        <div class="col s12 m12">
+            <div class="card">
+                <div class="card-content">
+                    <h5>Realisations</h5>
+                    <div class="divider"></div>
+                    <table class="responsive-table highlight">
+                        <thead>
+                        <tr>
+                            <th>Realisation</th>
+                            <th>Lien</th>
+                            <th>Date</th>
+                            <th>Description</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        <?php foreach ($realisations as $realisation) {?>
+                        <tr>
+                            <td><?php echo ucfirst($realisation->nomRealisation);?></td>
+                            <td><?php echo ucfirst($realisation->lienRealisation);?></td>
+                            <td><?php echo ucfirst($realisation->dateRealisation);?></td>
+                            <td><?php echo ucfirst($realisation->descriptionRealisation);?></td>
+                            <td>
+                                <a class="btn-floating blue darken-1 center" href="modif_realisation.html"><i class="material-icons">edit</i></a>
+                                <a class="btn-floating red darken-1 center" href=""><i class="material-icons">delete_sweep</i></a>
+                            </td>
+                        </tr>
+                        <?php }?>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+        <div class="col s12 m12">
+            <div class="card">
+                <div class="card-content">
+                    <h5>Emplois</h5>
+                    <div class="divider"></div>
+                    <table class="responsive-table highlight">
+                        <thead>
+                        <tr>
+                            <th>Poste</th>
+                            <th>Entreprise</th>
+                            <th>Duree</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        <?php foreach ($emplois as $emploi) {?>
+                        <tr>
+                            <td><?php echo ucfirst($emploi->posteEmplois);?></td>
+                            <td><?php echo ucfirst($emploi->nomEntreprise);?></td>
+                            <td><?php echo ucfirst($emploi->dateDebutEmplois);?>-<?php echo ucfirst($emploi->dateFinEmplois);?></td>
+                            <td>
+                                <a class="btn-floating blue darken-1 center" href="modif_emploi.html"><i class="material-icons">edit</i></a>
+                                <a class="btn-floating red darken-1 center" href=""><i class="material-icons">delete_sweep</i></a>
+                            </td>
+                        </tr>
+                        <?php }?>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
