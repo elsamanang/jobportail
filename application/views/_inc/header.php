97e6fe973930740256945fc0f265e2efd180ffd9
Roland Beni <roland.beni1@gmail.com>
2019-03-20 12:40:18 +0200

    [ADD] defined site default header

---
+++ application/views/_inc/header.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assets/materialise.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>/assets/font.css">
+    <script src="<?php echo base_url();?>/assets/materialize.js"></script>
+    <link rel="stylesheet" href="<?php echo base_url();?>/assets/style.css">
+    <title>Job Portal</title>
+</head>
+<body>
+<div class="row">
+    <nav class="blue-grey">
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo"><i class="material-icons large left" id="title-icon">work</i> Job-Portal</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li class="active"><a href="#"><i class="material-icons large left" id="">home</i></a></li>
+                <li><a href="<?php echo site_url('login');?>">Login</a></li>
+                <li><a href="<?php echo site_url('inscription');?>">Inscription</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
