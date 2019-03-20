3b68dbb84932f543d44910380a3f93d399696911
Roland Beni <roland.beni1@gmail.com>
2019-03-20 12:58:21 +0200

    [UPDATE] define le menu a afficher

--- application/views/_inc/header.php
+++ application/views/_inc/header.php
@@ -16,8 +16,18 @@
             <a href="<?php echo site_url('accueil');?>" class="brand-logo"><i class="material-icons large left" id="title-icon">work</i> Job-Portal</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li class="active"><a href="<?php echo site_url('accueil');?>"><i class="material-icons large left" id="">home</i></a></li>
-                <li><a href="<?php echo site_url('login');?>">Login</a></li>
-                <li><a href="<?php echo site_url('inscription');?>">Inscription</a></li>
+                <?php
+                    if(!isset($this->session->user)){
+                ?>
+                    <li><a href="<?php echo site_url('login');?>">Login</a></li>
+                    <li><a href="<?php echo site_url('inscription');?>">Inscription</a></li>
+                <?php }?>
+                <?php
+                    if(isset($this->session->user)){
+                ?>
+                    <li><a href="<?php echo site_url('profile');?>">Profil</a></li>
+                    <li><a href="<?php echo site_url('logout');?>">Deconnexion</a></li>
+                <?php }?>
             </ul>
         </div>
     </nav>
