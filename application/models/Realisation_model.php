6e69a14457014ef1bc7a382860274836e51782bd
Roland Beni <roland.beni1@gmail.com>
2019-03-21 01:23:28 +0200

    [UPDATE]methode get_by_id_user

--- application/models/Realisation_model.php
+++ application/models/Realisation_model.php
@@ -28,6 +28,12 @@ class Realisation_model extends CI_Model
         $this->db->where($this->id, $id);
         return $this->db->get($this->table)->row();
     }
+    // get data by idUser
+    function get_by_id_user($id)
+    {
+        $this->db->where('fk_idDemandeur', $id);
+        return $this->db->get($this->table)->result();
+    }
     
     // get total rows
     function total_rows($q = NULL) {
