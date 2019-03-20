df7413cac3d1d9da03e6bb3761299eaaf40e94da
Roland Beni <roland.beni1@gmail.com>
2019-03-21 01:25:22 +0200

    [UPDATE] entreprise_profile view structure

---
+++ application/views/profile_entreprise.php
@@ -0,0 +1,40 @@
+<h4 class="center blue-grey-text"><?php echo ucfirst($title);?></h4>
+<div class="row">
+    <div class="col s3">
+        <div class="col s12 m8 offset-m2">
+            <div class="card">
+                <div class="card-image">
+                    <img src="assets/img.png">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col s8 card">
+        <div class="col s7">
+            <h5>Identity</h5>
+            <div class="divider"></div>
+            <p class="blue-grey-text">Nom : <span>Pbreakers</span></p>
+            <p class="blue-grey-text">Pseudo : <span>pbreakers</span></p>
+            <p class="blue-grey-text">Adresse : <span>Lubumbashi</span></p>
+            <p class="blue-grey-text">Email : <span>pbreakers@gmail.com</span></p>
+            <p class="blue-grey-text">Telephone : <span>+243991550495</span></p>
+            <p class="blue-grey-text">Site : <span>www.pbreakers.com</span></p>
+            <p class="blue-grey-text">Code Postal : <span>00243</span></p>
+            <p class="blue-grey-text">Fax : <span>---</span></p>
+            <p class="blue-grey-text">Nationalite : <span>Zairoise</span></p>
+            <p class="blue-grey-text">Etat civil : <span>Mariee</span></p>
+        </div>
+        <div class="col s4 offset-s1">
+            <div class="row">
+                <p class="">
+                    <a class="waves-effect waves-light btn right" href="<?php echo site_url('add_offre')?>"><i class="material-icons left">add</i>Ajouter Offre</a>
+                </p>
+            </div>
+            <div class="row">
+                <p class="">
+                    <a class="waves-effect waves-light btn right" href="<?php echo site_url('list_offre')?>"><i class="material-icons left">view_headline</i>Liste Offres</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
