9ca04dfa12da05059294a6da86d308b9c4193af6
Roland Beni <roland.beni1@gmail.com>
2019-03-21 12:45:21 +0200

    [UPDATE] define new accueil routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,8 +49,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
-$route['accueil'] = 'welcome';
+$route['default_controller'] = 'welcome/login';
+$route['accueil_user'] = 'user/welcome';
+$route['accueil_entreprise'] = 'entrerise/welcome';
 $route['login'] = 'welcome/login';
 $route['logout'] = 'welcome/logout';
 $route['log'] = 'welcome/login_action';
