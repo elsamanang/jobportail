71d4c6d320d128f7204da592d57285f363ee5f5c
Roland Beni <roland.beni1@gmail.com>
2019-03-22 20:48:15 +0200

    [UPDATE] put values on view eprofile

--- application/views/profile_entreprise.php
+++ application/views/profile_entreprise.php
@@ -4,7 +4,9 @@
         <div class="col s12 m8 offset-m2">
             <div class="card">
                 <div class="card-image">
-                    <img src="assets/img.png">
+                    <img src="<?php echo base_url()?><?php 
+                        $img = ($this->session->entreprise->logo == NULL) ? "assets/none.jpg" : $this->session->entreprise->logo;
+                            echo $img;?>">
                 </div>
             </div>
         </div>
@@ -13,16 +15,14 @@
         <div class="col s7">
             <h5>Identity</h5>
             <div class="divider"></div>
-            <p class="blue-grey-text">Nom : <span>Pbreakers</span></p>
-            <p class="blue-grey-text">Pseudo : <span>pbreakers</span></p>
-            <p class="blue-grey-text">Adresse : <span>Lubumbashi</span></p>
-            <p class="blue-grey-text">Email : <span>pbreakers@gmail.com</span></p>
-            <p class="blue-grey-text">Telephone : <span>+243991550495</span></p>
-            <p class="blue-grey-text">Site : <span>www.pbreakers.com</span></p>
-            <p class="blue-grey-text">Code Postal : <span>00243</span></p>
-            <p class="blue-grey-text">Fax : <span>---</span></p>
-            <p class="blue-grey-text">Nationalite : <span>Zairoise</span></p>
-            <p class="blue-grey-text">Etat civil : <span>Mariee</span></p>
+            <p class="blue-grey-text">Nom : <span><?php echo ucfirst($this->session->entreprise->nomEmployeur)?></span></p>
+            <p class="blue-grey-text">Adresse : <span><?php echo ucfirst($this->session->entreprise->adresseEmployeur)?></span></p>
+            <p class="blue-grey-text">Email : <span><?php echo $this->session->entreprise->emailEmployeur?></span></p>
+            <p class="blue-grey-text">Telephone : <span><?php echo $this->session->entreprise->telephoneEmployeur?></span></p>
+            <p class="blue-grey-text">Site : <span><?php echo $this->session->entreprise->siteEmployeur?></span></p>
+            <p class="blue-grey-text">Code Postal : <span><?php echo $this->session->entreprise->codePostal?></span></p>
+            <p class="blue-grey-text">Fax : <span><?php echo $this->session->entreprise->fax?></span></p>
+            <p class="blue-grey-text">Pseudo : <span><?php echo $this->session->entreprise->pseudo?></span></p>
         </div>
         <div class="col s4 offset-s1">
             <div class="row">
@@ -32,8 +32,11 @@
             </div>
             <div class="row">
                 <p class="">
-                    <a class="waves-effect waves-light btn right" href="<?php echo site_url('list_offre')?>"><i class="material-icons left">view_headline</i>Liste Offres</a>
+                    <a class="waves-effect waves-light btn right" href="<?php echo site_url('offres')?>"><i class="material-icons left">view_headline</i>Liste Offres</a>
                 </p>
+            </div>
+            <div class="row">
+            <p><a class="waves-effect waves-light btn right" href="<?php echo site_url('modif_eprofile')?>/<?php echo $this->session->entreprise->idEmployeur?>"><i class="material-icons right">edit</i>Modifier</a></p>
             </div>
         </div>
     </div>
