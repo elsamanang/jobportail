1d124caee3c65d09bab6e6300883effb3f162e48
Roland Beni <roland.beni1@gmail.com>
2019-03-22 13:45:52 +0200

    [UPDATE] update the search =>join with table employeur

--- application/models/Offre_model.php
+++ application/models/Offre_model.php
@@ -44,11 +44,12 @@ class Offre_model extends CI_Model
     function get_limit_data($limit, $start = 0, $q = NULL) {
         $this->db->order_by($this->id, $this->order);
         $this->db->like('idOffre', $q);
-	$this->db->or_like('posteOffre', $q);
-	$this->db->or_like('dateDebutOffre', $q);
-	$this->db->or_like('dateFinOffre', $q);
-	$this->db->or_like('fk_idEmployeur', $q);
-	$this->db->limit($limit, $start);
+        $this->db->or_like('posteOffre', $q);
+        $this->db->or_like('dateDebutOffre', $q);
+        $this->db->or_like('dateFinOffre', $q);
+        $this->db->or_like('fk_idEmployeur', $q);
+        $this->db->join('employeur', 'offre.fk_idEmployeur = employeur.idEmployeur');
+        $this->db->limit($limit, $start);
         return $this->db->get($this->table)->result();
     }
 
