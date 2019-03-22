64f1fb6d0fb6855047e346035bdb1492d5d67b54
Roland Beni <roland.beni1@gmail.com>
2019-03-22 20:47:04 +0200

    [UPDATE] mis en ordre de tous les routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,87 +1,56 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/*
-| -------------------------------------------------------------------------
-| URI ROUTING
-| -------------------------------------------------------------------------
-| This file lets you re-map URI requests to specific controller functions.
-|
-| Typically there is a one-to-one relationship between a URL string
-| and its corresponding controller class/method. The segments in a
-| URL normally follow this pattern:
-|
-|	example.com/class/method/id/
-|
-| In some instances, however, you may want to remap this relationship
-| so that a different class/function is called than the one
-| corresponding to the URL.
-|
-| Please see the user guide for complete details:
-|
-|	https://codeigniter.com/user_guide/general/routing.html
-|
-| -------------------------------------------------------------------------
-| RESERVED ROUTES
-| -------------------------------------------------------------------------
-|
-| There are three reserved routes:
-|
-|	$route['default_controller'] = 'welcome';
-|
-| This route indicates which controller class should be loaded if the
-| URI contains no data. In the above example, the "welcome" class
-| would be loaded.
-|
-|	$route['404_override'] = 'errors/page_missing';
-|
-| This route will tell the Router which controller/method to use if those
-| provided in the URL cannot be matched to a valid route.
-|
-|	$route['translate_uri_dashes'] = FALSE;
-|
-| This is not exactly a route, but allows you to automatically route
-| controller and method names that contain dashes. '-' isn't a valid
-| class or method name character, so it requires translation.
-| When you set this option to TRUE, it will replace ALL dashes in the
-| controller and method URI segments.
-|
-| Examples:	my-controller/index	-> my_controller/index
-|		my-controller/my-method	-> my_controller/my_method
-*/
+//Acceuils
 $route['default_controller'] = 'welcome/login';
 $route['accueil_user'] = 'user/welcome';
 $route['accueil_entreprise'] = 'entreprise/welcome';
+
+//login ang logout
 $route['login'] = 'welcome/login';
-$route['logout'] = 'welcome/logout';
 $route['log'] = 'welcome/login_action';
+$route['logout'] = 'welcome/logout';
+
+//Inscriptions
+$route['inscription'] = 'welcome/inscription';
 $route['logupPersonne'] = 'user/demandeur/create_action';
 $route['logupEntreprise'] = 'entreprise/employeur/create_action';
-$route['inscription'] = 'welcome/inscription';
+
+//Profiles user(demandeur)
 $route['uprofile'] = 'user/demandeur/profile';
 $route['modif_uprofile/(:any)'] = 'user/demandeur/update/$1';
 $route['update_uprofile'] = 'user/demandeur/update_action';
-$route['eprofile'] = 'entreprise/employeur/profile';
 
+//Profiles entreprises(employeur)
+$route['eprofile'] = 'entreprise/employeur/profile';
+$route['modif_eprofile/(:any)'] = 'entreprise/employeur/update/$1';
+$route['update_eprofile'] = 'entreprise/employeur/update_action';
+
+//Offres
 $route['offres'] = 'entreprise/offre';
 $route['add_offre'] = 'entreprise/offre/create';
 $route['addoffre'] = 'entreprise/offre/create_action';
+
+//Realisations
 $route['addrealisation'] = 'user/realisation/create_action';
-$route['addcompetences'] = 'user/competences/create_action';
-$route['addemplois'] = 'user/emplois/create_action';
-$route['addformation'] = 'user/formation/create_action';
-
-$route['modif_uformation/(:any)'] = 'user/formation/update/$1';
-$route['modif_ucompetence/(:any)'] = 'user/competences/update/$1';
 $route['modif_urealisation/(:any)'] = 'user/realisation/update/$1';
-$route['modif_uemplois/(:any)'] = 'user/emplois/update/$1';
-
-$route['update_uformation'] = 'user/formation/update_action';
-$route['update_ucompetences'] = 'user/competences/update_action';
 $route['update_urealisation'] = 'user/realisation/update_action';
+
+//Competences
+$route['addcompetences'] = 'user/competences/create_action';
+$route['modif_ucompetence/(:any)'] = 'user/competences/update/$1';
+$route['update_ucompetences'] = 'user/competences/update_action';
+
+//Emplois
+$route['addemplois'] = 'user/emplois/create_action';
+$route['modif_uemplois/(:any)'] = 'user/emplois/update/$1';
 $route['update_uemplois'] = 'user/emplois/update_action';
 
+//Formations
+$route['addformation'] = 'user/formation/create_action';
+$route['modif_uformation/(:any)'] = 'user/formation/update/$1';
+$route['update_uformation'] = 'user/formation/update_action';
 
-$route['deconnexion'] = 'welcome/logout';
+//Reserved(errors,...)
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
