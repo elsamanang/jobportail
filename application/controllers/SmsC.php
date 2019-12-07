5834669acd9ee6409c8feb45efe0d5049f1e22bb
Roland Beni <roland.beni1@gmail.com>
2019-12-07 15:27:58 +0200

    [UPDATE] nom du controller sms
    Signed-off-by: Roland Beni <roland.beni1@gmail.com>

--- application/controllers/SmsC.php
+++ application/controllers/SmsC.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Sms extends CI_Controller
+class SmsC extends CI_Controller
 {
 	public function __construct(){
 		parent::__construct();
@@ -10,7 +10,8 @@ class Sms extends CI_Controller
 	{
 
 		$this->load->library('twilio');
-		$sms_sender = trim($this->input->post('sms_sender'));
+		$sms_sender = '+12562853071'; //trim($this->input->post('sms_sender'));
+//		var_dump($this->input->post('sms_recipient'));die();
 		$sms_reciever = $this->input->post('sms_recipient');
 		$sms_message = trim($this->input->post('sms_message'));
 		$from = '+' . $sms_sender; //trial account twilio number
