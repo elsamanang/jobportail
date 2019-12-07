b2cb81a271bb1b103e90bf89b0d204a3100fac1b
Roland Beni <roland.beni1@gmail.com>
2019-12-07 14:38:46 +0200

    [ADD] twilio config
    Signed-off-by: Roland Beni <roland.beni1@gmail.com>

---
+++ application/config/twilio.php
@@ -0,0 +1,27 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['mode'] = 'sandbox';
+
+/**
+ * Account SID
+ **/
+$config['account_sid'] = 'ACce861a23c03b99ebaaa3652e99123be3';
+
+/**
+ * Auth Token
+ **/
+$config['auth_token'] = '30a9936807760efa1887c1f8ed0e9111';
+
+/**
+ * API Version
+ **/
+$config['api_version'] = '2010-04-01';
+
+/**
+ * Twilio Phone Number
+ **/
+$config['number'] = 'Your configuration no that you have need to set in twilio dashboard is used to send recive message';
+
+/* End of file twilio.php */
+?>
